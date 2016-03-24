f86bf5efb6cf92f5d5f09fbe0625e684902f37d2
Andrew Rosen <retsej@gatech.edu>
2016-03-24 14:32:35 -0400

    Might want a linter.  This is getting larger

--- simulation.py
+++ simulation.py
@@ -1,4 +1,15 @@
+class Simulator(object):
+    self.events = [] # queue of time  
+    self.nodes = {}  # Key object
+    self.numNodes = 100
+    self.nodes = []
+    self.numtasks = 10000
+
+class Event(object):
+    pass
+
 class Node(object):
+    self.id  = -1
     self.backups = {}  # (key, value) -> (Original Owner, [Keys])   
     self.shortPeers =  []
     self.longPeers = []
