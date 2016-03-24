6d3195dccf0d8c8b655abad9dfbd4fa535ddb0b7
Andrew Rosen <retsej@gatech.edu>
2016-03-24 14:45:26 -0400

    test

--- simulation.py
+++ simulation.py
@@ -1,17 +1,23 @@
 class Simulator(object):
-    self.events = [] # queue of time  
-    self.nodes = {}  # Key object
-    self.numNodes = 100
-    self.nodes = []
-    self.numtasks = 10000
+    events = [] # queue of time  
+    nodes = {}  # Key object
+    
+    numNodes = 100
+    nodes = []
+    
+    numtasks = 10000
 
 class Event(object):
     pass
 
 class Node(object):
-    self.id  = -1
-    self.backups = {}  # (key, value) -> (Original Owner, [Keys])   
-    self.shortPeers =  []
-    self.longPeers = []
+    def __init__(self):
+        self.id  = -1
+        self.backups = {}  # (key, value) -> (Original Owner, [Keys])   
+        self.shortPeers =  []
+        self.longPeers = []
+        self.files = {} # files[hashkey] = value , collisions overwrite (S.O.P.)
     
-    
+    def store(self,key , value):
+        self.files[key] = value
+        # do backup 
