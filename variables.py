b4e13ae65e26366adf4d5c35845c751f3f6a9de2
Andrew Rosen <rosen@cs.gsu.edu>
2016-04-10 18:27:14 -0400

    culling variables

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # integrated variables 
-trials = 1
+trials = 100
 strategies = [ "churn", "randomInjection", "neighbors", "invite"]
 homogeneity = ["equal", "randomUniform"] #"randomGauss" ]
 workPerTick = ["one", "perSybil", "perStrength"]
@@ -21,14 +21,13 @@ randomGauss: Nodes have a random
 
 
 
-networkSizes = [10, 50, 100, 500, 1000, 5000, 10000]
-jobSizes = [100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 10000000]
-churnRates = [0, 0.00001, 0.0001, 0.001, 0.01]
+networkSizes = [1000, 5000, 10000]
+jobSizes = [100000, 500000, 1000000] #10000000
+churnRates = [0, 0.0001, 0.001, 0.01]
 adaptationRates = [1, 5, 10]
-sybilThresholds = [0, 0.01, 0.1, 0.25] 
-
-maxSybils = [1,5,10]
-successors = [5,10,20]
+maxSybils = [5,10]
+sybilThresholds = [0, 0.1, 0.25] 
+successors = [5,10]
 
 # unintegrated variables
 
