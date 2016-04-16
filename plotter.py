fafaa35db0592c5408990a4a4e95ce8dce12938c
Andrew Rosen <rosen@cs.gsu.edu>
2016-04-16 19:10:11 -0400

    writing plotter

---
+++ plotter.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import math
+from builder import generateFileIDs, MAX
+
+def drawGraph(nodes, tasks):
+    xs = []
+    ys = [] 
+    fx = []
+    fy = []
+    for _ in range(nodes):
+        n = next(generateFileIDs())
+        x = math.sin(2*math.pi*n/ MAX)
+        y = math.cos(2*math.pi*n/ MAX)
+        xs.append(x)
+        ys.append(y)
+    
+    for _ in range(tasks):
+        n = next(generateFileIDs())
+        x = math.sin(2*math.pi*n/ MAX)
+        y = math.cos(2*math.pi*n/ MAX)
+        fx.append(x)
+        fy.append(y)
+    #plt.axes([-1.0,1.0,-1.0,1.0] )
+    plt.plot(xs,ys, 'ro')
+    plt.plot(fx,fy, 'bo')
+    plt.show()
+
+def drawAverageChurn(filename):
+    data =  open("data/done/"+filename+".txt")
+    for line in data:
+        print(line)
+
+
+drawAverageChurn("averagesChurn1k1m")
