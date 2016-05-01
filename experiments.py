c12e86e0abaa5c8ebf705af6b7caacb165e9577c
Andrew Rosen <rosen@cs.gsu.edu>
2016-05-01 09:57:29 -0400

    readyToRun

--- experiments.py
+++ experiments.py
@@ -266,7 +266,7 @@ if __name__ == '__main__':
 
     startTime = time.time()
     
-    runInviteLimitedSize(100, 10000)
+    runInviteNoChurn(100, 10000)
     #runChurnLimitedSize(100, 100000)
     end= time.time()
     print("Time elapsed:" + str(end - startTime))
